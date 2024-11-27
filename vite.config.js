06b07bd8c5e4ed32dc388203e9e2506fb455cbe5
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-11-27 21:23:15 +0530

    git page changes22

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Phone_Book/', // For project sites
-
+  base: '/Phone_Book/', // Correct base path for GitHub Pages
   plugins: [react()],
-})
+});
